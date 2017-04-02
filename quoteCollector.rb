438368eac373c403b44bbe6fa2b3b81108bce2b2
EDWIN AU <edwinau@rogers.com>
2017-04-02 15:10:15 -0400

    Quote Collector complete

--- quoteCollector.rb
+++ quoteCollector.rb
@@ -1,4 +1,13 @@
 require_relative "multilinguist"
 
 class quoteCollector < Multilinguist
+  
+  def quotes_memorized(sentence)
+    x = 0
+    sentence.each do |y|
+      x << y
+      end
+      self.say_in_local_language("Here's a quote for you #{rand(x)}")
+    end
+
 end
